9f34786de63277820fc027ad2dbfca1b2f7a2186
Fabio Lima <flrlima1991@gmail.com>
2023-06-02 19:00:20 -0300

    feat: create token jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,9 +6,9 @@ import {
 	UsePipes,
 	ValidationPipe
 } from '@nestjs/common';
-import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
+import { ReturnLoginDto } from './dtos/returnLogin.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -17,7 +17,7 @@ export class AuthController {
 
   @UsePipes(ValidationPipe)
   @Post()
-  async login(@Body() loginDto: LoginDto): Promise< ReturnUserDto> {
-	return new ReturnUserDto(await this.authService.login(loginDto))
+  async login(@Body() loginDto: LoginDto): Promise<ReturnLoginDto> {
+	return (await this.authService.login(loginDto))
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: String(process.env.JWT_SECRET),
+        signOptions: { expiresIn: String(process.env.JWT_EXPIRES_IN) }
+      })
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })

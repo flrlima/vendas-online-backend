56c41a96600fb477f512f495152110a4f3097fe3
Fabio Lima <flrlima1991@gmail.com>
2023-07-04 18:10:42 -0300

    feat: create roles jwt

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line prettier/prettier
 import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressEntity } from './entities/address.entity';
@@ -8,11 +10,12 @@ import { AddressEntity } from './entities/address.entity';
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
+  @Roles(UserType.User)
   @Post('/:userId')
   @UsePipes(ValidationPipe)
   async createAddress(
     @Body() createAddressDto: CreateAddressDto,
-    @Param('userId') userId: number
+    @Param('userId') userId: number,
   ): Promise<AddressEntity> {
     return this.addressService.createAddress(createAddressDto, userId);
   }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { StateModule } from './state/state.module';
-import { CityModule } from './city/city.module';
 import { AddressModule } from './address/address.module';
-import { CacheModule } from './cache/cache.module';
 import { AuthModule } from './auth/auth.module';
+import { CacheModule } from './cache/cache.module';
+import { CityModule } from './city/city.module';
+import { RolesGuard } from './guards/roles.guard';
+import { StateModule } from './state/state.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: ['.env.development.local']
+      envFilePath: ['.env.development.local'],
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
@@ -22,16 +25,22 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.DB_USERNAME,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
       migrations: [`${__dirname}/migration/{.ts,*.js}`],
-      migrationsRun: true
+      migrationsRun: true,
     }),
     UserModule,
     StateModule,
     CityModule,
     AddressModule,
     CacheModule,
-    AuthModule
+    AuthModule,
+    JwtModule,
   ],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
